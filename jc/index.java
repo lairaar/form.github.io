677d425cb59c8fef56f2d5a50ff4e2ce2270448b
lairaar <lairaarruda@gmail.com>
2017-10-01 22:31:32 -0300

    Update index.java

--- jc/index.java
+++ jc/index.java
@@ -1,1 +1,6 @@
-.
+.function verifica(aceitou) {
+      if (aceitou.checked)
+        document.getElementById('enviar').disabled = false;
+      else
+        document.getElementById('enviar').disabled = true;
+    }
