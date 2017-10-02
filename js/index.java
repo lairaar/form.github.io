f8aa670aa40113807504d6b3d08f9688975fad63
lairaar <lairaarruda@gmail.com>
2017-10-02 20:30:16 -0300

    Update index.java

--- js/index.java
+++ js/index.java
@@ -4,3 +4,13 @@
       else
         document.getElementById('enviar').disabled = true;
     }
+
+.(function mostrar()) {
+      if (mostrar.checked) {
+        senha.type="text";
+        senha2.type="text";
+      } else {
+        senha.type="password";
+        senha2.type="password";
+      }
+    }
